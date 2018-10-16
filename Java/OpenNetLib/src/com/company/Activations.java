6d1501f8766eafc95563ea4549c593611287ed10
BenHenderson09 <37810700+BenHenderson09@users.noreply.github.com>
2018-10-16 19:08:51 +0100

    Added sigmoid activation function

--- Java/OpenNetLib/src/com/company/Activations.java
+++ Java/OpenNetLib/src/com/company/Activations.java
@@ -3,12 +3,20 @@ package com.company;
 public class Activations {
 
     // -----------------------------  Hyperbolic Tangent ------------------------------------
-
     static double tanh (double x){
         return Math.tanh(x);
     }
 
     static double tanhDeriv(double x){
         return x = 1-(Math.pow(x, 2));
+    }
+    
+    // -----------------------------  Sigmoid ------------------------------------
+    static double sigmoid (double x){ 
+        return 1/(1+Math.exp(-x));
+    }
+
+    static double sigDeriv (double x){ 
+        return  x*(1-x);
     }
 }
